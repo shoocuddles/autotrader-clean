5ec5678a7818ab0b28e8a1a65fcffe5c1d033359
shoocuddles <6352910@gmail.com>
2025-03-25 05:34:12 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const urls = [
 
 app.get('/scrape', async (req, res) => {
   const browser = await puppeteer.launch({
-    headless: false, // Force headed mode
+    headless: "new",
     executablePath: executablePath(),
     args: [
       '--no-sandbox',
@@ -23,7 +23,6 @@ app.get('/scrape', async (req, res) => {
 
   const page = await browser.newPage();
 
-  // Fake a real browser
   await page.setUserAgent(
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
   );
@@ -36,7 +35,6 @@ app.get('/scrape', async (req, res) => {
       await page.goto(url, { waitUntil: 'domcontentloaded' });
       await new Promise(resolve => setTimeout(resolve, 3000));
 
-      // Scroll the page to simulate user interaction
       for (let i = 0; i < 3; i++) {
         await page.evaluate(() => window.scrollBy(0, window.innerHeight));
         await new Promise(resolve => setTimeout(resolve, 1000));
